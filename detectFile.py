7ac2a39f084caa8ca7f8f7efe8719a7c7a68f267
YozNacks <duarte.a.arribas@gmail.com>
2022-11-08 17:32:23 +0000

    added hashes instead of files | added email sending

--- detectFile.py
+++ detectFile.py
@@ -1,14 +1,19 @@
 import sqlite3
 import os
+import smtplib
+import keyring
+import hashlib
+from email.mime.text import MIMEText
 from os import listdir
 from os.path import isfile, join
 
 # Global variables
-DATABASE  = "db/detectFiles.db"
-IN_FOLDER = "in"
+DATABASE   = "db/detectFiles.db"
+IN_FOLDER  = "in"
+FROM_EMAIL = ""
+TO_EMAIL   = ""
 
-
-def getFiles(dir):
+def getFileHashes(dir):
   """Get files from a directory.
 
   Parameters
@@ -19,12 +24,21 @@ def getFiles(dir):
   Returns
   -------
   lst
-      List of files in the directory
+      List of file hashes in the directory
   """
-  return [f for f in os.listdir(dir) if isfile(join(dir,f))]
+  filenameList = []
+  hashList     = []
+  hashing      = hashlib.sha256()
+  for f in os.listdir(dir):
+    if(isfile(join(dir,f))):
+      with open(join(dir,f),"r"):
+        hashing.update(f.encode('utf-8'))
+      hashList.append(hashing.hexdigest())
+      filenameList.append(f)
+  return (filenameList,hashList)
 
 
-def getNewFiles(con,cur,files):
+def getNewFiles(con,cur,filenames,fileHashes):
   """Check if the given files already exist in the database. If not, consider them new.
 
   Parameters
@@ -41,20 +55,44 @@ def getNewFiles(con,cur,files):
   lst
       A list of new files
   """
-  filesNotInDB = []
-  res          = cur.execute("SELECT filename FROM previousFiles")
-  filesInDb    = res.fetchall()
-  for f in files:
-    if f not in [filesInDbNormalized[0] for filesInDbNormalized in filesInDb]:
-      cur.execute("INSERT INTO previousFiles VALUES(?)",(f,))
+  filesNotInDB  = []
+  res           = cur.execute("SELECT fileHash FROM previousFiles")
+  hashesInDb    = res.fetchall()
+  for i in range(len(filenames)):
+    if fileHashes[i] not in [hashesInDbNormalized[0] for hashesInDbNormalized in hashesInDb]:
+      cur.execute("INSERT INTO previousFiles VALUES(?)",(fileHashes[i],))
       con.commit()
-      filesNotInDB.append(f)
+      filesNotInDB.append(filenames[i])
   return filesNotInDB
+  
+def emailNewFiles(newFiles):
+  """Emails new files to the specified email
+
+  Parameters
+  ----------
+  newFiles : str
+      the contents of the new files
+  """
+  server = smtplib.SMTP("smtp-mail.outlook.com",587)
+  server.connect("smtp-mail.outlook.com",587)
+  server.ehlo()
+  server.starttls()
+  server.ehlo()
+  server.login(FROM_EMAIL,keyring.get_password("system","EMAIL_TO_SEND"))
+  msg = MIMEText(F"New files available! -{newFiles}")
+  msg["Subject"] = "You've got new files!"
+  server.sendmail(FROM_EMAIL,TO_EMAIL,msg.as_string())
+  server.quit()
   
 def main():
   # Open database connection
   con = sqlite3.connect(DATABASE)
   cur = con.cursor()
+  # If there are new files, email them
+  files = getFileHashes(IN_FOLDER)
+  newFiles = getNewFiles(con,cur,files[0],files[1])
+  if(newFiles):
+    emailNewFiles(newFiles)
   
 if __name__ == '__main__':
   main()
--- test_detectFile.py
+++ test_detectFile.py
@@ -1,22 +1,16 @@
-from detectFile import getFiles
-from detectFile import check
+from detectFile import getFileHashes
+from detectFile import getNewFiles
 import unittest
 import sqlite3
 
 class TestDetectFile(unittest.TestCase):
   def test_get_files(self):
-    self.assertEqual(getFiles("inTest/1"),[])
-  
-  def test_get_files2(self):
-    self.assertEqual(getFiles("inTest/2"),["1.txt","2.txt","3.txt"])
-  
-  def test_get_files3(self):
-    self.assertEqual(getFiles("inTest/3"),["1.txt","2.txt"])
+    self.assertEqual(getFileHashes("inTest/1"),[])
     
   def test_check(self):
     con = sqlite3.connect("db/detectFiles.db")
     cur = con.cursor()
-    self.assertEqual(check(con,cur,["file7.txt","file6.txt","file5.txt"]),["file7.txt","file6.txt","file5.txt"])
+    self.assertEqual(getNewFiles(con,cur,["file7.txt","file6.txt","file5.txt"]),["file7.txt","file6.txt","file5.txt"])
     
 if __name__ == '__main__':
   unittest.main()
