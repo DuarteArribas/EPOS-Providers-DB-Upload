763d183ba6e4bbe316dadd0f6cb7af12644f38d6
YozNacks <duarte.a.arribas@gmail.com>
2022-12-21 00:45:00 +0000

    separated file from path

--- validate.py
+++ validate.py
@@ -69,45 +69,45 @@ def validateMetadataLine(line,file):
   value    = " ".join(line.split(" ")[1:])
   if header == "ReferenceFrame":
     if value not in ["IGS08","IGS14","free-network","IGb08","INGV_EU"]:
-      return False,f"Wrong ReferenceFrame - '{value}' in file {file}."
+      return False,f"Wrong ReferenceFrame - '{value}' in file {file.split('/')[-1]}, with path: {file}."
   elif header == "EpochOfFrame":
     if not validateDate(value):
-      return False,f"Wrong EpochOfFrame format - '{value}' in file {file}."
+      return False,f"Wrong EpochOfFrame format - '{value}' in file {file.split('/')[-1]}, with path: {file}."
   elif header == "CovarianceMatrix":
     if value not in ["full","block-diagonal","diagonal"]:
-      return False,f"Wrong CovarianceMatrix - '{value}' in file {file}."
+      return False,f"Wrong CovarianceMatrix - '{value}' in file {file.split('/')[-1]}, with path: {file}."
   elif header == "AnalysisCentre" or header == "CombinationCentre":
     if value not in ["UGA","INGV","WUT-EUREF","BFHK","ROB-EUREF"]:
-      return False,f"Wrong AnalysisCentre/CombinationCentre - '{value}' in file {file}."
+      return False,f"Wrong AnalysisCentre/CombinationCentre - '{value}' in file {file.split('/')[-1]}, with path: {file}."
   elif header == "Software":
     if value not in ["Bernese GNSS Software 5.2","GIPSY-OASIS","CATREF"]:
-      return False,f"Wrong Software - '{value}' in file {file}."
+      return False,f"Wrong Software - '{value}' in file {file.split('/')[-1]}, with path: {file}."
   elif header == "SINEX_version":
     if not isFloat(value):
-      return False,f"Wrong SINEX_version format - '{value}' in file {file}."
+      return False,f"Wrong SINEX_version format - '{value}' in file {file.split('/')[-1]}, with path: {file}."
   elif header == "CutOffAngle":
     if not value.isdigit():
-      return False,f"Wrong CutOffAngle format - '{value}' in file {file}."
+      return False,f"Wrong CutOffAngle format - '{value}' in file {file.split('/')[-1]}, with path: {file}."
   elif header == "OTLModel":
     if value not in ["FES2004","GOT4.10c","FES2014b"]:
-      return False,f"Wrong OTLModel - '{value}' in file {file}."
+      return False,f"Wrong OTLModel - '{value}' in file {file.split('/')[-1]}, with path: {file}."
   elif header == "AntennaModel" or header == "Antennamodel":
     if value not in ["epn_14_1958.atx","igs08_wwww.atx","epn_14.atx"]:
-      return False,f"Wrong AntennaModel - '{value}' in file {file}."
+      return False,f"Wrong AntennaModel - '{value}' in file {file.split('/')[-1]}, with path: {file}."
   elif header == "DOI":
     if not value:
-      return False,f"Wrong DOI format - '{value}' in file {file}."
+      return False,f"Wrong DOI format - '{value}' in file {file.split('/')[-1]}, with path: {file}."
   elif header == "CreationDate":
     if not validateDate(value):
-      return False,f"Wrong CreationDate format - '{value}' in file {file}."
+      return False,f"Wrong CreationDate format - '{value}' in file {file.split('/')[-1]}, with path: {file}."
   elif header == "ReleaseNumber":
     if not value.isdigit():
-      return False,f"Wrong ReleaseNumber format - '{value}' in file {file}."
+      return False,f"Wrong ReleaseNumber format - '{value}' in file {file.split('/')[-1]}, with path: {file}."
   elif header == "SamplingPeriod":
     if value not in ["daily","weekly"]:
-      return False,f"Wrong SamplingPeriod - '{value}' in file {file}."
+      return False,f"Wrong SamplingPeriod - '{value}' in file {file.split('/')[-1]}, with path: {file}."
   else:
-    return False,f"Wrong metadata paremeter - '{header}' of value '{value}' in file {file}."
+    return False,f"Wrong metadata paremeter - '{header}' of value '{value}' in file {file.split('/')[-1]}, with path: {file}."
   return True,"No problem"
   
 def isFloat(num):
