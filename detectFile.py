e3f378e3981cbec29a7d796175f37bec1128df90
duarte.a.arribas@gmail.com <duarte.a.arribas@gmail.com>
2023-01-13 16:39:23 +0000

    changed moveToPublic function to have the correct version name and uncommented email sending

--- detectFile.py
+++ detectFile.py
@@ -101,7 +101,7 @@ def moveToPublic(providerDir,publicDir):
       lines = f.readlines()
       lines = [line.strip() for line in lines]
       for line in lines[lines.index("+FILE/COMMENT") + 1:lines.index("-FILE/COMMENT")]:
-        if line.split(" ")[0] == "SINEX_version":
+        if line.split(" ")[0] == "ReleaseNumber":
           version = "".join(line.split(" ")[1:])
           break
     coorOrTs = "TS" if "TS" in file else "Coor" 
@@ -122,50 +122,50 @@ def main():
       if validate:
         moveToPublic(providerDir,PUBLIC_DIR["INGV"])
       else:
-        #sendEmail(
-        #  f"Validation failure (requires attention in {provider}) | {datetime.now().strftime('%d/%m/%Y - %H:%M:%S')}",
-        #  f"{validationError}"
-        #)
+        sendEmail(
+          f"Validation failure (requires attention in {provider}) | {datetime.now().strftime('%d/%m/%Y - %H:%M:%S')}",
+          f"{validationError}"
+        )
         pass
     if provider == "ROB" and hashesChanged[1]:
       validate,validationError = validateProviderDir(providerDir)
       if validate:
         moveToPublic(providerDir,PUBLIC_DIR["ROB"])
       else:
-        #sendEmail(
-        #  f"Validation failure (requires attention in {provider}) | {datetime.now().strftime('%d/%m/%Y - %H:%M:%S')}",
-        #  f"{validationError}"
-        #)
+        sendEmail(
+          f"Validation failure (requires attention in {provider}) | {datetime.now().strftime('%d/%m/%Y - %H:%M:%S')}",
+          f"{validationError}"
+        )
         pass
     if provider == "SGO" and hashesChanged[2]:
       validate,validationError = validateProviderDir(providerDir)
       if validate:
         moveToPublic(providerDir,PUBLIC_DIR["SGO"])
       else:
-        #sendEmail(
-        #  f"Validation failure (requires attention in {provider}) | {datetime.now().strftime('%d/%m/%Y - %H:%M:%S')}",
-        #  f"{validationError}"
-        #)
+        sendEmail(
+          f"Validation failure (requires attention in {provider}) | {datetime.now().strftime('%d/%m/%Y - %H:%M:%S')}",
+          f"{validationError}"
+        )
         pass
     if provider == "UGA" and hashesChanged[3]:
       validate,validationError = validateProviderDir(providerDir)
       if validate:
         moveToPublic(providerDir,PUBLIC_DIR["UGA"])
       else:
-        #sendEmail(
-        #  f"Validation failure (requires attention in {provider}) | {datetime.now().strftime('%d/%m/%Y - %H:%M:%S')}",
-        #  f"{validationError}"
-        #)
+        sendEmail(
+          f"Validation failure (requires attention in {provider}) | {datetime.now().strftime('%d/%m/%Y - %H:%M:%S')}",
+          f"{validationError}"
+        )
         pass
     if provider == "WUT" and hashesChanged[4]:
       validate,validationError = validateProviderDir(providerDir)
       if validate:
         moveToPublic(providerDir,PUBLIC_DIR["WUT"])
       else:
-        #sendEmail(
-        #  f"Validation failure (requires attention in {provider}) | {datetime.now().strftime('%d/%m/%Y - %H:%M:%S')}",
-        #  f"{validationError}"
-        #)
+        sendEmail(
+          f"Validation failure (requires attention in {provider}) | {datetime.now().strftime('%d/%m/%Y - %H:%M:%S')}",
+          f"{validationError}"
+        )
         pass
   
 if __name__ == '__main__':
