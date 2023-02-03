1e80bd050c2c3bf2797627102cf47c18da87605e
duarte.a.arribas@gmail.com <duarte.a.arribas@gmail.com>
2023-02-03 17:07:05 +0000

    added connect function to DBConnection class

--- src/dbConnection.py
+++ src/dbConnection.py
@@ -1,9 +1,27 @@
+import psycopg2
+
 class DBConnection:
   """A database connection."""
   
   # == Methods ==
-  def __init__(self):
+  def __init__(self,host,port,databaseName,username,password):
     """Init needed database parameters
     """
+    self.host         = host
+    self.port         = port
+    self.databaseName = databaseName
+    self.username     = username
+    self.password     = password
+    self.conn         = None
+    self.cursor       = None
   
+  def connect(self):
+    self.conn = psycopg2.connect(
+      host     = self.host,
+      port     = self.port,
+      database = self.databaseName,
+      user     = self.username,
+      password = self.password
+    )
+    self.cursor = self.conn.cursor()
   
