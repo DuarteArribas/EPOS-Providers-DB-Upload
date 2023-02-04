4c09d705e75095f7c5b80de06d025b9c5a8a48e8
duarte.a.arribas@gmail.com <duarte.a.arribas@gmail.com>
2023-02-04 01:02:51 +0000

    added optimized insert

--- src/databaseUpload.py
+++ src/databaseUpload.py
@@ -59,3 +59,35 @@ class TSDatabaseUpload:
     except Exception as err:
       print("Error: Could not connect to database: \n" + str(err),file=sys.stderr)
       self.cursor.execute("ROLLBACK TRANSACTION")
+  
+  def uploadSolutionOptimized(
+    self,
+    solutionFile
+  ):
+    self.cursor.execute("BEGIN TRANSACTION")
+    try:
+      with open(solutionFile,"r") as csvFile:
+        self.cursor.copy_expert(
+          f"""
+          COPY solution(
+            solution_type,
+            ac_acronym,
+            creation_date,
+            software,
+            doi,
+            url,
+            ac_name,
+            version,
+            reference_frame,
+            processing_parameters_url,
+            sampling_period
+          )
+          FROM STDIN
+          WITH (FORMAT CSV,HEADER TRUE);
+          """,
+          csvFile
+        )
+      self.cursor.execute("COMMIT TRANSACTION")
+    except Exception as err:
+      print("Error: Could not connect to database: \n" + str(err),file=sys.stderr)
+      self.cursor.execute("ROLLBACK TRANSACTION")
--- tests/test_TSdatabaseUpload.py
+++ tests/test_TSdatabaseUpload.py
@@ -3,12 +3,17 @@ from src.dbConnection   import *
 from src.databaseUpload import *
 
 class TestTDDatabaseUpload(unittest.TestCase):
-  def test_upload_station(self):
+  def test_upload_solution(self):
     pgConnection = DBConnection("localhost","5432","arroztestDB","postgres","arroz123")
     pgConnection.connect()
     tsUpload     = TSDatabaseUpload(pgConnection.conn,pgConnection.cursor)
-    tsUpload.uploadSolution('ARROZANDMASSA','massa','1111-12-11','a','b','c','d','a','b','c','d')
-    
+    tsUpload.uploadSolution('WTF','massa','1111-12-11','a','b','c','d','a','b','c','d')
+  
+  def test_upload_solution_optimized(self):
+    pgConnection = DBConnection("localhost","5432","arroztestDB","postgres","arroz123")
+    pgConnection.connect()
+    tsUpload     = TSDatabaseUpload(pgConnection.conn,pgConnection.cursor)
+    tsUpload.uploadSolutionOptimized("in/solution_tmp_file")
     
 if __name__ == '__main__':
   unittest.main()
