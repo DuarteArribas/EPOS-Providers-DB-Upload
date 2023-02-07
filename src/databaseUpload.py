147bf1a895cfc8678f2a083ff076939f960ae564
duarte.a.arribas@gmail.com <duarte.a.arribas@gmail.com>
2023-02-07 18:03:08 +0000

    optimized the whole process

--- src/databaseUpload.py
+++ src/databaseUpload.py
@@ -1,5 +1,7 @@
 import psycopg2
+import shutil
 import glob
+import gzip
 import sys
 import os
 from src.utils.constants import *
@@ -8,11 +10,16 @@ from src.utils.logs      import *
 class TSDatabaseUpload:
   """Upload TS solutions to the database."""
   
+  # == Class variables ==
+  ANALYSIS_CENTRE = {"INGV":"ING","UGA-CNRS":"UGA","WUT-EUREF":"EUR","ROB-EUREF":"ROB","SGO-EPND":"SGO"}
+  SOLUTION_TMP    = "solutionTmp.csv"
+  
   # == Methods ==
-  def __init__(self,conn,cursor,logger):
+  def __init__(self,conn,cursor,logger,tmp):
     self.conn   = conn
     self.cursor = cursor
     self.logger = logger
+    self.tmpDir = tmp + "/" if tmp[-1] != "/" else tmp
   
   def uploadAllTS(self,publicDir):
     allTSFiles = self._getListOfTSFiles(publicDir)
@@ -30,6 +37,47 @@ class TSDatabaseUpload:
   def _saveInformationToFile(self,tsFile):
     self._saveSolutionToFile(tsFile)
     # TODO: Save the rest
+  
+  def _saveSolutionToFile(self,tsFile):
+    solutionParameters = self._getSolutionParameters(tsFile)
+    with open(os.path.join(self.tmpDir,TSDatabaseUpload.SOLUTION_TMP),"a") as tmp:
+      tmp.write(
+        str(solutionParameters["solution_type"])   + "," +
+        str(solutionParameters["ac_acronym"])      + "," +
+        str(solutionParameters["creation_date"])   + "," +
+        str(solutionParameters["software"])        + "," +
+        str(solutionParameters["doi"])             + "," +
+        str(solutionParameters["url"])             + "," +
+        str(solutionParameters["ac_name"])         + "," +
+        str(solutionParameters["version"])         + "," +
+        str(solutionParameters["reference_frame"]) + "," +
+        str(solutionParameters["sampling_period"]) + "\n"
+      )
+    
+  def _getSolutionParameters(self,tsFile):
+    with gzip.open(tsFile,"rt") as f:
+      lines = [line.strip() for line in f.readlines()]
+      solutionParameters = {"solution_type":"TS"}
+      for line in lines[lines.index("%Begin EPOS metadata") + 1:lines.index("%End EPOS metadata")]:
+        match [el.strip() for el in line.split(":",1)]:
+          case ["ReferenceFrame",value]:
+            solutionParameters["reference_frame"] = value
+          case ["AnalysisCentre",value]:
+            solutionParameters["ac_name"]    = value
+            solutionParameters["ac_acronym"] = TSDatabaseUpload.ANALYSIS_CENTRE[value]
+          case ["Software",value]:
+            solutionParameters["software"] = value
+          case ["Method-url",value]:
+            solutionParameters["url"] = value
+          case ["DOI",value]:
+            solutionParameters["doi"] = value
+          case ["ReleaseNumber",value]:
+            solutionParameters["version"] = value
+          case ["SamplingPeriod",value]:
+            solutionParameters["sampling_period"] = value
+          case ["CreationDate",value]:
+            solutionParameters["creation_date"] = value
+      return solutionParameters
     
   def _uploadTSOptimized(self):
     try:
@@ -37,16 +85,20 @@ class TSDatabaseUpload:
       self._uploadSolutionOptimized()
       # TODO: Upload more
       self.cursor.execute("COMMIT TRANSACTION")
+      self._removeFilesInDir(self.tmpDir)
     except Exception as err:
       self.cursor.execute("ROLLBACK TRANSACTION")
+      self._removeFilesInDir(self.tmpDir)
       raise Exception(err)
   
-  def _uploadSolutionOptimized(
-    self,
-    solutionFile
-  ):
+  def _removeFilesInDir(self,dir):
+    files = glob.glob(f"{dir}/*")
+    for f in files:
+      os.remove(f)
+  
+  def _uploadSolutionOptimized(self):
     try:
-      with open(solutionFile,"r") as csvFile:
+      with open(os.path.join(self.tmpDir,TSDatabaseUpload.SOLUTION_TMP),"r") as csvFile:
         self.cursor.copy_expert(
           f"""
           COPY solution(
@@ -59,11 +111,10 @@ class TSDatabaseUpload:
             ac_name,
             version,
             reference_frame,
-            processing_parameters_url,
             sampling_period
           )
           FROM STDIN
-          WITH (FORMAT CSV,HEADER TRUE);
+          WITH (FORMAT CSV,HEADER FALSE);
           """,
           csvFile
         )
@@ -74,27 +125,13 @@ class TSDatabaseUpload:
   def _uploadTS(self,file):
     try:
       self.cursor.execute("BEGIN TRANSACTION")
-      self._uploadSolution()
+      self._uploadSolution(self._getSolutionParameters(file),file)
       # TODO: Upload more
       self.cursor.execute("COMMIT TRANSACTION")
     except:
       self.cursor.execute("ROLLBACK TRANSACTION")
   
-  def _uploadSolution(
-    self,
-    solution_type,
-    ac_acronym,
-    creation_date,
-    software,
-    doi,
-    url,
-    ac_name,
-    version,
-    reference_frame,
-    processing_parameters_url,
-    sampling_period,
-    filename
-  ):
+  def _uploadSolution(self,solutionParameters,filename):
     try:
       self.cursor.execute(
         f"""
@@ -108,21 +145,19 @@ class TSDatabaseUpload:
           ac_name,
           version,
           reference_frame,
-          processing_parameters_url,
           sampling_period
         )
         VALUES(
-          '{solution_type}',
-          '{ac_acronym}',
-          '{creation_date}',
-          '{software}',
-          '{doi}',
-          '{url}',
-          '{ac_name}',
-          '{version}',
-          '{reference_frame}',
-          '{processing_parameters_url}',
-          '{sampling_period}'
+          '{solutionParameters["solution_type"]}',
+          '{solutionParameters["ac_acronym"]}',
+          '{solutionParameters["creation_date"]}',
+          '{solutionParameters["software"]}',
+          '{solutionParameters["doi"]}',
+          '{solutionParameters["url"]}',
+          '{solutionParameters["ac_name"]}',
+          '{solutionParameters["version"]}',
+          '{solutionParameters["reference_frame"]}',
+          '{solutionParameters["sampling_period"]}'
         )
         """
       )
--- tests/test_TSdatabaseUpload.py
+++ tests/test_TSdatabaseUpload.py
@@ -1,23 +1,50 @@
 import unittest
+from src.utils.logs     import *
 from src.dbConnection   import *
 from src.databaseUpload import *
 
 class TestTDDatabaseUpload(unittest.TestCase):
+  #def test_getSolutionParameters(self):
+  #  tsUpload = TSDatabaseUpload("dummy","dummy2","dummy3","dummy4")
+  #  print(tsUpload._getSolutionParameters("outTest/public/INGV/5/TS/arroz.pos.gz"))
+  #  
+  #def test_writeSolutionParameters(self):
+  #  tsUpload = TSDatabaseUpload("dummy","dummy2","dummy3","tmp")
+  #  print(tsUpload._saveSolutionToFile("outTest/public/INGV/5/TS/arroz.pos.gz"))
+  #
+  #def test_upload_solution_opt(self):
+  #  pgConnection = DBConnection("localhost","5432","arroztestDB","postgres","arroz123")
+  #  pgConnection.connect()
+  #  logger = Logs("logs/logsTest.log",10000)
+  #  tsUpload     = TSDatabaseUpload(pgConnection.conn,pgConnection.cursor,logger,"tmp")
+  #  pgConnection.cursor.execute("BEGIN TRANSACTION")
+  #  tsUpload._uploadSolutionOptimized()
+  #  pgConnection.cursor.execute("COMMIT TRANSACTION")
+  #
+  #def test_getTSFiles(self):
+  #  tsUpload = TSDatabaseUpload("dummy","dummy2","dummy3","dummy4")
+  #  self.assertEqual([file.split("/")[-1] for file in tsUpload._getListOfTSFiles("outTest/public")].sort(),["a.pos","b.vel","aab.pos","pp.vel","1.pos","a2.pos","a3.vel","b2.vel","p9.vel","arroz.pos.gz"].sort())
+  #
   #def test_upload_solution(self):
   #  pgConnection = DBConnection("localhost","5432","arroztestDB","postgres","arroz123")
   #  pgConnection.connect()
-  #  tsUpload     = TSDatabaseUpload(pgConnection.conn,pgConnection.cursor)
-  #  tsUpload.uploadSolution('WTF','massa','1111-12-11','a','b','c','d','a','b','c','d')
+  #  logger = Logs("logs/logsTest.log",10000)
+  #  tsUpload     = TSDatabaseUpload(pgConnection.conn,pgConnection.cursor,logger,"tmp")
+  #  tsUpload._uploadSolution('WTF','massa','1111-12-11','a','b','c','d','a','b','c',"arroz")
   #
-  #def test_upload_solution_optimized(self):
+  #def test_remove_from_dir(self):
   #  pgConnection = DBConnection("localhost","5432","arroztestDB","postgres","arroz123")
   #  pgConnection.connect()
-  #  tsUpload     = TSDatabaseUpload(pgConnection.conn,pgConnection.cursor)
-  #  tsUpload.uploadSolutionOptimized("in/solution_tmp_file")
-  
-  def test_getTSFiles(self):
-    tsUpload = TSDatabaseUpload("dummy","dummy2","dummy3")
-    self.assertEqual([file.split("/")[-1] for file in tsUpload._getListOfTSFiles("outTest/public")],["a.pos","b.vel","aab.pos","pp.vel","1.pos","a2.pos","a3.vel","b2.vel","p9.vel"])
+  #  logger = Logs("logs/logsTest.log",10000)
+  #  tsUpload     = TSDatabaseUpload(pgConnection.conn,pgConnection.cursor,logger,"tmp")
+  #  tsUpload._removeFilesInDir("tmp")
+  # 
+  def test_upload_TS_opt(self):
+    pgConnection = DBConnection("localhost","5432","arroztestDB","postgres","arroz123")
+    pgConnection.connect()
+    logger = Logs("logs/logsTest.log",10000)
+    tsUpload     = TSDatabaseUpload(pgConnection.conn,pgConnection.cursor,logger,"tmp")
+    tsUpload._uploadTSOptimized()
     
 if __name__ == '__main__':
   unittest.main()
