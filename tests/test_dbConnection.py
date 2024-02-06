43363ede56bd4df332db9f44c46e634ec174cc55
DuarteArribas <duarte.a.arribas@gmail.com>
2024-02-06 12:30:25 +0000

    added test dbConnection

--- tests/test_dbConnection.py
+++ tests/test_dbConnection.py
@@ -2,12 +2,9 @@ import unittest
 from src.dbConnection import *
 
 class TestDBConnection(unittest.TestCase):
-  def test_connection_unsuccess(self):
-    pgConnection = DBConnection("localhost","12345","arroz","arroz","arroz")
-    pgConnection.connect()
-    
   def test_connection_success(self):
-    pgConnection = DBConnection("localhost","5432","postgres","postgres","arroz123")
+    logger       = Logs("logs/logsTest.log",1000)
+    pgConnection = DBConnection("localhost","5432","db305","postgres","postgres",logger)
     pgConnection.connect()
     
 if __name__ == '__main__':
